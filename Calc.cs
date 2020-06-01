3dc71d976ae1332f7d0d790e2001e7b736a02906
JonS <jsmartware@aol.com>
2020-06-01 12:52:10 -0700

    Add project files.

---
+++ Calc.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    public static class Calc
+    {
+        
+        // Method uses generic "Func<T1,T2,O3>" delegate
+        // It takes in "Left" and "right" (first two params) , and outputs the result in third param.
+        private static Func<decimal, decimal, decimal> GetOperation(char oper)
+        {
+            switch (oper)
+            {
+                case '+': return (l, r) => l + r;  // Note the use of lambda expressions
+                case '-': return (l, r) => l - r;
+                case '*': return (l, r) => l * r;
+                case '/': return (l, r) => l / r;
+                default: throw new NotSupportedException("The supplied operator is not supported");
+            }
+        }
+
+        public static decimal Eval(string expr)
+        {
+            var elements = expr.Split(new[] { ' ' }, 3);
+            var l = Decimal.Parse(elements[0]);
+            var r = Decimal.Parse(elements[1]);
+            var op = elements[2][0];  // +, -, *, /
+
+            return GetOperation(op)(l, r); // calls method with generic Func<>
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+
+
+namespace Calculator
+{
+    class Program
+    {
+        // This calc uses Reverse Polish Notation to calculate results
+        // Two decimal numbers are passed in with one of four basic math operations
+        // The static Calc class uses Functional Programming to return results.
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Calc.Eval("1 3 +"));
+            Console.WriteLine(Calc.Eval("10 5 -"));
+            Console.WriteLine(Calc.Eval("2 3 *"));
+            Console.WriteLine(Calc.Eval("14 2 /"));
+
+        }
+    }
+}
